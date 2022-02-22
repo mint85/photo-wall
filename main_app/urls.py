c79940fbda87ac07bc3806f3ee1c4ebceea6eea9
Allen Jones <allen.jones@gmail.com>
2022-02-22 18:57:06 +0000

    adds photo create, update, and delete functionality as well as forms to display for these tasks

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('photos/', views.photos_index, name='photos_index'),
     path('photos/<int:photo_id>/', views.photos_detail, name='photo_detail'),
+    path('photos/create/', views.PhotoCreate.as_view(), name='photos_create'),
+    path('photos/<int:pk>/update/', views.PhotoUpdate.as_view(), name='photos_update'),
+    path('photos/<int:pk>/delete/', views.PhotoDelete.as_view(), name='photos_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from lib2to3.pygram import python_grammar_no_print_statement
+# from lib2to3.pygram import python_grammar_no_print_statement
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Photo
 
 # Define the home view
@@ -15,3 +17,19 @@ def photos_detail(request, photo_id):
     #if photo.user_id != request.user.id:
         #return redirect('photos_index')
     return render(request, 'photos/detail.html', { 'photo': photo }) 
+
+class PhotoCreate(CreateView):
+    model = Photo
+    fields = ('name', 'description')
+
+    def get_absolute_url(self):
+        return reverse('photos_index')
+
+class PhotoUpdate(UpdateView):
+    model = Photo
+    fields = ('name', 'description')
+
+class PhotoDelete(DeleteView):
+    model = Photo
+    success_url = '/photos/'
+
